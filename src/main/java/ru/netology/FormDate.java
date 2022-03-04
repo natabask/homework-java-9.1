5b9fe06450cbd7a47b1d6fca805b989d916de9c3
natabask <natabask@yandex.ru>
2022-03-04 18:41:51 +0300

    initial commit

---
+++ src/main/java/ru/netology/FormDate.java
@@ -0,0 +1,7 @@
+package ru.netology;
+
+public class FormDate {
+    public int day; // поле для дня из даты рождения
+    public int month; // поле для месяца из даты рождения
+    public int year; // поле для года из даты рождения
+}
---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,11 @@
+package ru.netology;
+
+public class Post {
+    public String name; // поле для имени
+    public String passport; // поле для серии и номера паспорта
+    public String patronymic; // поле для отчества
+    public String phone;// поле номера телефона
+    public String surname; // поле для фамилии
+    public boolean subscription; // поле для согласия на смс-рассылку
+    public FormDate birthday; // поле для даты рождения дд.мм.гггг
+}
